2611fd771b23da68ae523f4bc400ae0e04d4e83d
cyberhardt <107224222+cyberhardt@users.noreply.github.com>
2023-05-31 07:13:00 -0300

    Add files via upload

---
+++ appvars.h
@@ -0,0 +1,11 @@
+#ifndef APPVARS_H
+#define APPVARS_H
+
+#include <QString>
+
+// Application variables
+#define APPNAME "fif"
+#define CURRVER 20180126
+#define APPVER "0.2.3"
+
+#endif // APPVARS_H
